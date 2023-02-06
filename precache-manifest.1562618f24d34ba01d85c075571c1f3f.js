35ed8455081ac30fd1ee6757364609fa87c0ad20
hhn27 <hoang7323@gmail.com>
2023-02-06 21:51:21 +0700

    Updates

---
+++ precache-manifest.1562618f24d34ba01d85c075571c1f3f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cea47ff639daa202d52",
+    "url": "./static/css/main.d8f1385a.chunk.css"
+  },
+  {
+    "revision": "2cea47ff639daa202d52",
+    "url": "./static/js/main.2b81b936.chunk.js"
+  },
+  {
+    "revision": "fe8ad015a4e319bf86db",
+    "url": "./static/js/runtime~main.c2497c15.js"
+  },
+  {
+    "revision": "925b77c4acda62b47e3c",
+    "url": "./static/js/2.cba83d94.chunk.js"
+  },
+  {
+    "revision": "554570e28cd96b792216",
+    "url": "./static/js/3.3796a966.chunk.js"
+  },
+  {
+    "revision": "2a8e41dbb8b07b20843fca67528a0a56",
+    "url": "./static/media/PIGAC2.2a8e41db.png"
+  },
+  {
+    "revision": "d96786c7df79e2229070add3fc170651",
+    "url": "./static/media/coll1.d96786c7.jpg"
+  },
+  {
+    "revision": "c359b9fa2b9083da6a62943c67726c39",
+    "url": "./static/media/coll2.c359b9fa.jpg"
+  },
+  {
+    "revision": "afbca8eb542487528f9f1295a8a8993d",
+    "url": "./static/media/coll3.afbca8eb.jpg"
+  },
+  {
+    "revision": "5bd4ced1a064bb6216b0113cefe859e8",
+    "url": "./static/media/banner.5bd4ced1.png"
+  },
+  {
+    "revision": "153b3c96d269fae4e2d2c491f8d6c365",
+    "url": "./index.html"
+  }
+];
