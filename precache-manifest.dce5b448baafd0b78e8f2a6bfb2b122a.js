45fed51ef1cce9b067261c57b04801a56046a3b7
hhn27 <hoang7323@gmail.com>
2023-02-06 22:16:18 +0700

    Updates

---
+++ precache-manifest.dce5b448baafd0b78e8f2a6bfb2b122a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "051358623d4bd29ca099",
+    "url": "/e-commerce/static/css/main.d8f1385a.chunk.css"
+  },
+  {
+    "revision": "051358623d4bd29ca099",
+    "url": "/e-commerce/static/js/main.95a3a29e.chunk.js"
+  },
+  {
+    "revision": "9190e0025d482a4c5c1f",
+    "url": "/e-commerce/static/js/runtime~main.98009de2.js"
+  },
+  {
+    "revision": "9024bfc66cd91a846d99",
+    "url": "/e-commerce/static/js/2.e5cda4d2.chunk.js"
+  },
+  {
+    "revision": "554570e28cd96b792216",
+    "url": "/e-commerce/static/js/3.3796a966.chunk.js"
+  },
+  {
+    "revision": "2a8e41dbb8b07b20843fca67528a0a56",
+    "url": "/e-commerce/static/media/PIGAC2.2a8e41db.png"
+  },
+  {
+    "revision": "d96786c7df79e2229070add3fc170651",
+    "url": "/e-commerce/static/media/coll1.d96786c7.jpg"
+  },
+  {
+    "revision": "c359b9fa2b9083da6a62943c67726c39",
+    "url": "/e-commerce/static/media/coll2.c359b9fa.jpg"
+  },
+  {
+    "revision": "afbca8eb542487528f9f1295a8a8993d",
+    "url": "/e-commerce/static/media/coll3.afbca8eb.jpg"
+  },
+  {
+    "revision": "5bd4ced1a064bb6216b0113cefe859e8",
+    "url": "/e-commerce/static/media/banner.5bd4ced1.png"
+  },
+  {
+    "revision": "19fd9d5420a5573a1d33fc9d2c69a52a",
+    "url": "/e-commerce/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5531502f342ceb44106903f818fdd1c4.js"
+  "/e-commerce/precache-manifest.dce5b448baafd0b78e8f2a6bfb2b122a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/e-commerce/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
